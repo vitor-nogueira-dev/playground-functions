58d0be2fd4403ca2ca47d5220ea4df4b922c523c
Vitor Nogueira <vslnogueira96@gmail.com>
2022-09-28 15:14:25 -0300

    Resolução do requisito [11 - bônus] Projeto Playground Functions

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,54 @@
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(numeros) {
+  if (numeros.length != 11) {
+    return 'Array com tamanho incorreto.';
+  }
+
+  for (let index of numeros) {
+    if (index < 0 || index > 9) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  for (let index of numeros) {
+    numeroRepetido = 0;
+    for (let i of numeros) {
+      if (index === i) {
+        numeroRepetido += 1;
+        if (numeroRepetido === 3) {
+          return 'não é possível gerar um número de telefone com esses valores';
+        }
+      }
+    }
+  }
+  return (
+    '(' +
+    numeros[0] +
+    '' +
+    numeros[1] +
+    ')' +
+    ' ' +
+    numeros[2] +
+    '' +
+    numeros[3] +
+    '' +
+    numeros[4] +
+    '' +
+    numeros[5] +
+    '' +
+    numeros[6] +
+    '-' +
+    numeros[7] +
+    '' +
+    numeros[8] +
+    '' +
+    numeros[9] +
+    '' +
+    numeros[10] +
+    ''
+  );
 }
+
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 7, 7, 8, 9, 3, 0, 2]));
 
 // Desafio 12
 function triangleCheck() {
